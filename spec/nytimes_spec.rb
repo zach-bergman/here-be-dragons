90ab1a29a1b80adc6b82b257aa55d406d5ad2742
Zach Bergman <bzach634@gmail.com>
2024-06-29 09:56:31 -0600

    Finished

--- spec/nytimes_spec.rb
+++ spec/nytimes_spec.rb
@@ -15,21 +15,24 @@ class Nytimes
       expect(result).to eq("OK")
     end
 
-    xit 'can get copyright' do 
+    it 'can get copyright' do 
       #Using @hash, define a variable called `result` that returns the copyright
+      result = @hash[:copyright]
 
       expect(result).to eq("Copyright (c) 2018 The New York Times Company. All Rights Reserved.")
     end
 
-    xit 'can get array of stories' do 
+    it 'can get array of stories' do 
       #Using @hash, define a variable called `result` that returns the array of stories
+      result = @hash[:results]
   
       expect(result).to be_an_instance_of(Array)
       expect(result.count).to eq(44)
     end
 
-    xit 'can get all stories with subsection of politics' do 
+    it 'can get all stories with subsection of politics' do 
       #Using @hash, define a variable called `result` that returns all stories with subsection of politics.
+      result = @hash[:results].select { |story| story[:subsection] == "Politics" }
   
       expect(result).to be_an_instance_of(Array)
       expect(result.count).to eq(6)
@@ -37,5 +40,4 @@ class Nytimes
       expect(result.last[:title]).to eq("Conspiracy Theories Made Alex Jones Very Rich. They May Bring Him Down.")
     end
   end 
-
 end
